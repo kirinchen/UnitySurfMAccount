b165814af384f644246df28889e066c21c66687e
kirinchen <a29243685@msn.com>
2018-03-11 22:17:50 +0800

    nick name modifyer script

---
+++ Scripts/NickNameModifyer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace com.surfm.account {
+    public class NickNameModifyer : MonoBehaviour {
+
+        private InputField nameInput;
+
+        void Awake() {
+            nameInput = GetComponentInChildren<InputField>();
+            nameInput.onEndEdit.AddListener(onEndEdit);
+        }
+
+        private void onEndEdit(string arg0) {
+            Account a = AccountLoader.getAccount();
+            a.userTitle = arg0;
+            AccountLoader.save();
+        }
+
+        void Start() {
+            Account a = AccountLoader.getAccount();
+            nameInput.text = a.userTitle;
+        }
+    }
+}
