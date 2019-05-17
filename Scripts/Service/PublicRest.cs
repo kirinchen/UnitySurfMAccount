2932ff0b742c881fa13a9f8e14e6fbaf720bd817
kirin <a29243004@gmail.com>
2019-05-18 00:08:24 +0800

    remove old code (not used)

--- Scripts/Service/PublicRest.cs
+++ Scripts/Service/PublicRest.cs
@@ -2,25 +2,31 @@
 using surfm.tool;
 
 using System;
+using UnityEngine;
 using static com.surfm.rest.URestApi;
 
 namespace com.surfm.account {
     public class PublicRest  {
 
         private static PublicRest instance;
+        private URestApi restApi = BeansRepo.bean<URestApi>(typeof(URestApi), "AccountServer");
 
-        private PublicRest() { }
+        private PublicRest() {
 
-         URestApi restApi = BeansRepo.bean<URestApi>(typeof(URestApi), "AccountServer");
+        }
+
 
 
         public void signUp(UserSignupFormDto dto, Action<Result> cb) {
-            string urlTemp = "/api/v1/public/signup";
+            string urlTemp = "api/v1/public/signup";
+
+            
+
             restApi.postJson(urlTemp, dto, cb);
         }
 
 
-        public PublicRest getInstance() {
+        public static PublicRest getInstance() {
             if (instance == null) {
                 instance = new PublicRest();
             }
---
+++ Scripts/Signuper.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static com.surfm.rest.URestApi;
+
+namespace com.surfm.account {
+    public class Signuper  {
+
+        private PublicRest publicRest = PublicRest.getInstance();
+        private Handler handler;
+        public Signuper(Handler s) {
+            handler = s;
+        }
+
+
+        public void signup() {
+
+            UserSignupFormDto dto = new UserSignupFormDto();
+            dto.email = handler.getEmail();
+            dto.password = handler.getPassword();
+
+            publicRest.signUp(dto, onResult);
+
+
+        }
+
+        private void onResult(Result r) {
+            Debug.Log("onResult: " + r.errorMsg);
+        }
+
+
+
+        public interface Handler {
+            string getEmail();
+            string getPassword();
+        }
+
+    }
+}
