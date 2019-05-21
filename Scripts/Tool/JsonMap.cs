d1645688eccfba88162550e8ea04e74c95e61297
kirin <a29243004@gmail.com>
2019-05-21 17:25:40 +0800

    add JsonMap

--- Scripts/Tool/JsonMap.cs
+++ Scripts/Tool/JsonMap.cs
@@ -1,28 +1,34 @@
-﻿using UnityEngine;
-using UnityEditor;
+﻿using System;
 using System.Collections.Generic;
-using System;
+using System.Reflection;
 
 namespace com.surfm.account {
-    public class JsonMap : Dictionary<string,object> {
+    public class JsonMap : Dictionary<string, object> {
 
 
 
-        public T toObj<T>(  T t) {
-
+        public T toObj<T>(T t, Func<FieldInfo, object, object> valueFunc = null) {
+            toObjRecursively(this, t, valueFunc);
             return t;
         }
 
 
+        public void toObjRecursively(Dictionary<string, object> d, object o, Func<FieldInfo, object, object> valueFunc = null) {
+            Type t = GetType();
+            FieldInfo[] fs = t.GetFields();
 
-        public void toObjRecursively<T>(Dictionary<string, object> d, T t) {
+            foreach (string ks in d.Keys) {
 
-            
+                object value = d[ks];
+                FieldInfo fd = t.GetField(ks);
+                if (value is Dictionary<string, object>) {
+                    toObjRecursively((Dictionary<string, object>)value, fd.GetValue(o), valueFunc);
+                } else {
+                    object _v = valueFunc == null ? value : valueFunc(fd, value);
+                    fd.SetValue(o, _v);
+                }
 
-
+            }
         }
-
-
-
     }
 }
