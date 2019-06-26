7ae7dc6b055e322969ff7813d4826ee6010d8e91
kirin <kirin.chen1001@gmail.com>
2019-06-26 12:41:00 +0800

    limt X Positiopn for camera

--- Scripts/Dto/UserSignupFormDto.cs
+++ Scripts/Dto/UserSignupFormDto.cs
@@ -1,11 +1,12 @@
-﻿using System.Collections;
+﻿using surfm.tool;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 namespace com.surfm.account {
     public class UserSignupFormDto : UserLoginFormDto {
 
         public string recaptchaResponse ="xxx";
-        public string phone ="091111111";
+        public string phone =UidUtils.getRandomNumber(10);
         public string skipCode = "ffff";
 
     }
